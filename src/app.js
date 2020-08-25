631fd547e0744cdb0ea0b3fc8be08ecd1cdebdc9
Des_Flex <denilay13902@gmail.com>
2020-08-25 22:12:33 +0500

    Dls comment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+// express сервер, который, логирует, парсит запросы
+// отдает статические файлы
+
 import express from 'express';
 import cors from 'cors'
 import path from 'path';
-import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv'
 
@@ -9,18 +11,13 @@ import indexRouter from './routes/index';
 
 dotenv.config();
 
-// const superData = {
-//   redux: 'included',
-//   iphone: 'fack',
-// }
-
 var app = express();
 app
   .use(cors())
   .use(logger('dev'))
   .use(express.json())
   .use(express.urlencoded({extended: true}))
-  .get('*',express.static(path.resolve(__dirname, '..', '..', 'logistics-management', 'build')))
+  .get('*',express.static(path.resolve(__dirname, '..', 'logistics-management', 'build')))
   .use('/', indexRouter)
   .use('/public', express.static(path.resolve(__dirname, '..', 'public')))
 
